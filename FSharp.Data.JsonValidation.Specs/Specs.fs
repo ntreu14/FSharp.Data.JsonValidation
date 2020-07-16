23d960ace91f1dac14aee308194c92ae4bc73b2f
Nicholas Treu <treunicholas@gmail.com>
2020-07-16 16:51:52 -0600

    Delete .Net framework projects and add dotnet core projects.

---
+++ FSharp.Data.JsonValidation.Specs/Specs.fs
@@ -0,0 +1,408 @@
+module FSharp.Data.JsonValidation.Specs
+
+open System
+open FSharp.Data
+open FSharp.Data.JsonValidation
+open NUnit.Framework
+
+let valid = function
+  | Valid _ -> ()
+  | result -> Assert.Fail (sprintf "Expected valid, got %A" result)
+
+let invalid = function
+  | Invalid _ -> ()
+  | result -> Assert.Fail (sprintf "Expected invalid, got %A" result)
+
+[<Test>]
+let ``the docs example works as written (a fat test, but bad code in docs is annoying)`` () =
+   let schema = ExactlyOneOf [JsonValue.String "hi"; JsonValue.Number 42M]
+   let value = JsonValue.String "goodbye"
+
+   Assert.AreEqual("Invalid \"Expected value to be one of [\"hi\"; 42] but was \"goodbye\"\"", sprintf "%A" (validate schema value))
+
+   valid <| validate schema (JsonValue.Number 42M)
+
+[<Test>]
+let ``the tutorial example works as written (a fat test, but bad code in docs is annoying)`` () =
+  let someNumbers = ArrayWhose [ItemsMatch AnyNumber]
+
+  let looksLikeAnEmail (value: string) =
+    value.Contains "@" && value.Contains "." && value.Length > 3
+
+
+  let person =
+    ObjectWhere
+      [
+        "name" .= StringThat [IsNotEmpty]
+        "favoriteNumbers" .= someNumbers
+        "email" .?= StringThat [IsEmail]
+      ]
+
+  let okayPerson =
+    """
+    {
+      "name": "Saul Goodman",
+      "favoriteNumbers": [747, 737000]
+    }
+    """
+
+  valid <| validate person (JsonValue.Parse okayPerson)
+
+  let emptyNamePerson =
+    """
+    {
+      "name": "",
+      "favoriteNumbers": [],
+      "email": "mysterious@haunted.house"
+    }
+    """
+
+  Assert.AreEqual("Invalid \".name Expected string to not be empty\"",  sprintf "%A" <| validate person (JsonValue.Parse emptyNamePerson))
+
+
+  let badEmail =
+    """
+    {
+      "name": "Jimmy M.",
+      "favoriteNumbers": [],
+      "email": "not-really-an-email"
+    }
+    """
+
+  Assert.AreEqual("Invalid \".email Expected not-really-an-email to be a valid email address\"", sprintf "%A" <| validate person (JsonValue.Parse badEmail))
+
+[<Test>]
+let ``Anything matches anything`` () =
+  valid <| validate Anything (JsonValue.String "hi")
+  valid <| validate Anything (JsonValue.Number 42M)
+  valid <| validate Anything (JsonValue.Array [||])
+  valid <| validate Anything (JsonValue.Record [||])
+
+[<Test>]
+let ``Not only validates when the given schema doesn't`` () =
+  invalid <| validate (Not AnyString) (JsonValue.String "hi")
+  valid <| validate (Not AnyString) (JsonValue.Number 42M)
+  valid <| validate (Not AnyString) (JsonValue.Array [||])
+  valid <| validate (Not AnyString) (JsonValue.Record [||])
+
+[<Test>]
+let ``Not negates itself`` () =
+  valid <| validate (Not (Not AnyString)) (JsonValue.String "hi")
+  invalid <| validate (Not (Not AnyString)) (JsonValue.Number 42M)
+  invalid <| validate (Not (Not AnyString)) (JsonValue.Array [||])
+  invalid <| validate (Not (Not AnyString)) (JsonValue.Record [||])
+
+[<Test>]
+let ``Exactly only matches exactly what it's given`` () =
+  valid <| validate (Exactly <| JsonValue.String "hi") (JsonValue.String "hi")
+  invalid <| validate (Exactly <| JsonValue.String "hi") (JsonValue.String "hi2")
+  invalid <| validate (Exactly <| JsonValue.String "hi") JsonValue.Null
+
+[<Test>]
+let ``ExactlyOneOf matches any of the values it's given`` () =
+  let schema = ExactlyOneOf [JsonValue.String "hi"; JsonValue.Number 42M]
+
+  valid <| validate schema (JsonValue.String "hi")
+  valid <| validate schema (JsonValue.Number 42M)
+  invalid <| validate schema JsonValue.Null
+  invalid <| validate schema (JsonValue.String "hi2")
+
+[<Test>]
+let ``AnythingBut matches anything except for the literals given`` () =
+  let schema = AnythingBut [JsonValue.String "hi"; JsonValue.Number 42M]
+
+  invalid <| validate schema (JsonValue.String "hi")
+  invalid <| validate schema (JsonValue.Number 42M)
+  valid <| validate schema JsonValue.Null
+  valid <| validate schema (JsonValue.String "hi2")
+
+[<Test>]
+let ``a JSON number is AnyNumber`` () =
+  valid <| validate AnyNumber (JsonValue.Number 42M)
+
+[<Test>]
+let ``something other than a JSON number is not AnyNumber`` () =
+  invalid <| validate AnyNumber (JsonValue.String "Foo")
+
+[<Test>]
+let ``a positive JSON number IsPositive`` () =
+  valid <| validate (NumberThat [IsPositive]) (JsonValue.Number 42M)
+  invalid <| validate (NumberThat [IsPositive]) (JsonValue.Number 0M)
+  invalid <| validate (NumberThat [IsPositive]) (JsonValue.Number -42M)
+
+[<Test>]
+let ``a negative JSON number IsNegative`` () =
+  invalid <| validate (NumberThat [IsNegative]) (JsonValue.Number 42M)
+  invalid <| validate (NumberThat [IsNegative]) (JsonValue.Number 0M)
+  valid <| validate (NumberThat [IsNegative]) (JsonValue.Number -42M)
+
+[<Test>]
+let ``a non-negative JSON number IsNonNegative`` () =
+  valid <| validate (NumberThat [IsNonNegative]) (JsonValue.Number 42M)
+  valid <| validate (NumberThat [IsNonNegative]) (JsonValue.Number 0M)
+  invalid <| validate (NumberThat [IsNonNegative]) (JsonValue.Number -42M)
+
+[<Test>]
+let ``an integral JSON number IsIntegral`` () =
+  valid <| validate (NumberThat [IsIntegral]) (JsonValue.Number 42M) 
+  valid <| validate (NumberThat [IsIntegral]) (JsonValue.Number 0M)
+  valid <| validate (NumberThat [IsIntegral]) (JsonValue.Number -42M)
+  invalid <| validate (NumberThat [IsIntegral]) (JsonValue.Number 42.94M)
+  invalid <| validate (NumberThat [IsIntegral]) (JsonValue.Number -42.94M)
+  valid <| validate (NumberThat [IsIntegral]) (JsonValue.Float 0.0)
+
+[<Test>]
+let ``a JSON number that is greater than another number IsGreaterThan`` () =
+  valid <| validate (NumberThat [IsGreaterThan 10M]) (JsonValue.Number 42M)
+  invalid <| validate (NumberThat [IsGreaterThan 10M]) (JsonValue.Number 0M)
+  invalid <| validate (NumberThat [IsGreaterThan 10M]) (JsonValue.Number 10M)
+
+[<Test>]
+let ``a JSON number that is less than another number IsLessThan`` () =
+  invalid <| validate (NumberThat [IsLessThan 10M]) (JsonValue.Number 42M)
+  valid <| validate (NumberThat [IsLessThan 10M]) (JsonValue.Number 0M)
+  invalid <| validate (NumberThat [IsLessThan 10M]) (JsonValue.Number 10M)
+
+[<Test>]
+let ``a JSON number can have multiple NumberThat properties`` () =
+  valid <| validate (NumberThat [IsGreaterThan 0M; IsPositive; IsIntegral]) (JsonValue.Number 42M)
+  valid <| validate (NumberThat [IsGreaterThan 0M; IsPositive; IsNonNegative]) (JsonValue.Number 42M)
+  invalid <| validate (NumberThat [IsLessThan 0M; IsNegative]) (JsonValue.Number 42M)
+  invalid <| validate (NumberThat [IsNegative; IsPositive]) (JsonValue.Number 42M)
+  invalid <| validate (NumberThat [IsNonNegative; IsGreaterThan -1M; IsNegative]) (JsonValue.Number 0M)
+
+[<Test>]
+let ``a JSON float will validate against NumberThat properties`` () = 
+  valid <| validate (NumberThat [IsGreaterThan 0M; IsPositive]) (JsonValue.Float 42.5)
+  valid <| validate (NumberThat [IsGreaterThan 0M; IsPositive; IsNonNegative]) (JsonValue.Float 42.5)
+  invalid <| validate (NumberThat [IsLessThan 0M; IsNegative]) (JsonValue.Float 42.5)
+  invalid <| validate (NumberThat [IsNegative; IsPositive]) (JsonValue.Float 42.5)
+  invalid <| validate (NumberThat [IsNonNegative; IsGreaterThan -1M; IsNegative]) (JsonValue.Float 0.0)
+
+[<Test>]
+let ``a JSON string is AnyString`` () =
+  valid <| validate AnyString (JsonValue.String "Hi")
+
+[<Test>]
+let ``something other than a JSON string is not AnyString`` () =
+  invalid <| validate AnyString (JsonValue.Number 42M)
+
+[<Test>]
+let ``StringThat [IsNotEmpty] matches non-empty strings`` () =
+  valid <| validate (StringThat [IsNotEmpty]) (JsonValue.String "hi")
+  invalid <| validate (StringThat [IsNotEmpty]) (JsonValue.String "")
+
+[<Test>]
+let ``AnyObject works`` () =
+  let schema = AnyObject
+
+  valid <| validate schema (JsonValue.Record [||])
+  invalid <| validate schema (JsonValue.Number 42M)
+  valid <| validate schema (JsonValue.Record [| "foo", JsonValue.String "bar" |])
+
+[<Test>]
+let ``ObjectWhere [...] must have required keys that match the given schema`` () =
+  let schema = ObjectWhere [ "foo" .= AnyString ]
+
+  invalid <| validate schema (JsonValue.Parse """ { "foo": 42 } """)
+  valid <| validate schema (JsonValue.Parse """ { "foo": "bar" } """)
+
+[<Test>]
+let ``ObjectWhere [...] can omit optional keys`` () =
+  let schema = ObjectWhere [ "foo" .?= AnyString ]
+
+  valid <| validate schema (JsonValue.Parse """ { } """)
+  valid <| validate schema (JsonValue.Parse """ { "foo": "bar" } """)
+
+[<Test>]
+let ``ObjectWhere [...] must meet the value schema when optional keys are given`` () =
+  let schema = ObjectWhere [ "foo" .?= AnyString ]
+
+  invalid <| validate schema (JsonValue.Parse """ { "foo": 42 } """)
+
+
+[<Test>]
+let ``ObjectWhere [...] ignores keys that aren't mentioned in the schema`` () =
+  let schema = ObjectWhere []
+
+  valid <| validate schema (JsonValue.Parse """ { "foo": 42 } """)
+
+
+[<Test>]
+let ``ArrayWhose [LengthIsAtLeast n] works`` () =
+  let schema = ArrayWhose [LengthIsAtLeast 4]
+
+  invalid <| validate schema (JsonValue.Array <| Array.replicate 3 JsonValue.Null)
+  valid <| validate schema (JsonValue.Array <| Array.replicate 4 JsonValue.Null)
+  valid <| validate schema (JsonValue.Array <| Array.replicate 5 JsonValue.Null)
+
+[<Test>]
+let ``AnyArray works`` () =
+  let schema = AnyArray
+
+  valid <| validate schema (JsonValue.Array [||])
+  invalid <| validate schema (JsonValue.Number 42M)
+  valid <| validate schema (JsonValue.Array [|JsonValue.String "foo"; JsonValue.Number 432M|])
+
+[<Test>]
+let ``ArrayWhose [LengthIsAtMost n] works`` () =
+  let schema = ArrayWhose [LengthIsAtMost 4]
+
+  valid <| validate schema (JsonValue.Array <| Array.replicate 3 JsonValue.Null)
+  valid <| validate schema (JsonValue.Array <| Array.replicate 4 JsonValue.Null)
+  invalid <| validate schema (JsonValue.Array <| Array.replicate 5 JsonValue.Null)
+
+[<Test>]
+let ``ArrayWhose [LengthIsExactly n] works`` () =
+  let schema = ArrayWhose [LengthIsExactly 4]
+
+  invalid <| validate schema (JsonValue.Array <| Array.replicate 3 JsonValue.Null)
+  valid <| validate schema (JsonValue.Array <| Array.replicate 4 JsonValue.Null)
+  invalid <| validate schema (JsonValue.Array <| Array.replicate 5 JsonValue.Null)
+
+[<Test>]
+let ``StringThat [IsLowerCase] works`` () =
+  let schema = StringThat [IsLowerCase]
+
+  valid <| validate schema (JsonValue.String "this is a test")
+  invalid <| validate schema (JsonValue.String "THIS IS A TEST")
+  invalid <| validate schema (JsonValue.String "This is a test")
+  invalid <| validate schema (JsonValue.String "this is a tesT")
+
+  invalid <| validate (StringThat [IsLowerCase; IsUpperCase]) (JsonValue.String "this is a test")
+
+[<Test>]
+let ``StringThat [IsUpperCase] works`` () =
+  let schema = StringThat [IsUpperCase]
+
+  invalid <| validate schema (JsonValue.String "this is a test")
+  valid <| validate schema (JsonValue.String "THIS IS A TEST")
+  invalid <| validate schema (JsonValue.String "This is a test")
+  invalid <| validate schema (JsonValue.String "this is a tesT")
+
+  invalid <| validate (StringThat [IsLowerCase; IsUpperCase]) (JsonValue.String "this is a test")
+
+[<Test>]
+let ``StringThat [MatchesCaseInsensitive] works`` () =
+  let schema = StringThat [MatchesCaseInsensitive "this is a test"]
+
+  valid <| validate schema (JsonValue.String "this is a test")
+  valid <| validate schema (JsonValue.String "THIS IS A TEST")
+  valid <| validate schema (JsonValue.String "tHiS iS a TeSt")
+
+  invalid <| validate schema (JsonValue.String "Completely different string")
+
+[<Test>]
+let ``StringThat [IsEmail] works`` () =
+  let schema = StringThat [IsEmail]
+ 
+  valid <| validate schema (JsonValue.String "test@example.com")
+  invalid <| validate schema (JsonValue.String "hello")
+  invalid <| validate schema (JsonValue.String "test@com")
+  invalid <| validate schema (JsonValue.String "test.com")
+  invalid <| validate schema (JsonValue.String "@.com")
+
+[<Test>] 
+let ``StringThat [IsAlphaNumeric] works`` () =
+  let schema = StringThat [IsAlphaNumeric]
+
+  valid <| validate schema (JsonValue.String "12345")
+  valid <| validate schema (JsonValue.String "foobar")
+  valid <| validate schema (JsonValue.String "foo123bar456")
+  
+  invalid <| validate schema (JsonValue.String String.Empty) 
+  invalid <| validate schema (JsonValue.String "foo bar")
+  invalid <| validate schema (JsonValue.String "foo_bar")
+  invalid <| validate schema (JsonValue.String "H3LL0 W0RLD!")
+  invalid <| validate schema (JsonValue.String "@(*#@(")
+
+[<Test>]
+let ``StringThat [IsGuid] works`` () = 
+  let schema = StringThat [IsGuid]
+  let guid = Guid.NewGuid()
+
+  valid <| validate schema (JsonValue.String <| string guid)
+  invalid <| validate schema (JsonValue.String "foo bar")
+
+[<Test>]
+let ``StringThat [IsMinimumLength] works`` () =
+  let schema n = StringThat [IsMinimumLength n]
+
+  valid <| validate (schema 7) (JsonValue.String "foo bar")
+  invalid <| validate (schema 8) (JsonValue.String "foo bar")
+  valid <| validate (schema 6) (JsonValue.String "foo bar")
+  invalid <| validate (schema -2) (JsonValue.String "foo bar")
+
+[<Test>]
+let ``StringThat [isMaximumLength] works`` () =
+  let schema n = StringThat [IsMaximumLength n]
+
+  valid <| validate (schema 7) (JsonValue.String "foo bar")
+  valid <| validate (schema 8) (JsonValue.String "foo bar")
+  invalid <| validate (schema 6) (JsonValue.String "foo bar")
+  invalid <| validate (schema -2) (JsonValue.String "foo bar") 
+
+[<Test>]
+let ``ArrayWhose [ItemsMatch schema] ensures items all match`` () =
+  let schema = ArrayWhose [ItemsMatch AnyNumber]
+
+  invalid <| validate schema (JsonValue.Array [|JsonValue.Null|])
+  invalid <| validate schema (JsonValue.Array [|JsonValue.Number 42M; JsonValue.Null|])
+  valid <| validate schema (JsonValue.Array [|JsonValue.Number 42M; JsonValue.Number 0M|])
+
+[<Test>]
+let ``when ArrayWhose [ItemsMatch schema] fails, it reports on the index of failure`` () =
+  let schema =
+    ObjectWhere
+      [
+        "b" .= ArrayWhose [ItemsMatch AnyNumber]
+      ]
+  
+  let result = validate schema (JsonValue.Parse """{ "b": [1, 2, "foo", 4] }""")
+
+  match result with
+    | Valid -> failwithf "Expected invalid"
+    | Invalid message -> Assert.That(message, Does.Contain ".b [2]")
+
+[<Test>]
+let ``Either [...] ensures any item matches`` () =
+  let schema = Either [Exactly <| JsonValue.Number 42M; AnyString]
+
+  valid <| validate schema (JsonValue.String "42")
+  invalid <| validate schema (JsonValue.Number 99M)
+  valid <| validate schema (JsonValue.Number 42M)
+
+[<Test>]
+let ``Delay matches allowing recursive data structures`` () =
+  let rec memory =
+    ObjectWhere
+      [
+        "subject" .= StringThat [IsNotEmpty]
+        "linkedMemory" .?= Delay (fun () -> memory)
+      ]
+
+  valid <| validate memory (JsonValue.Parse """ { "subject": "Lone Memory" } """)
+
+  """
+  {
+    "subject": "I link",
+    "linkedMemory": {
+      "subject": "I'm linked to"
+    }
+  }
+  """
+    |> JsonValue.Parse
+    |> validate memory
+    |> valid
+
+  """
+  {
+    "subject": "I link",
+    "linkedMemory": {
+      "subject": 42
+    }
+  }
+  """
+    |> JsonValue.Parse
+    |> validate memory
+    |> invalid
---
+++ FSharp.Data.JsonValidation/Library.fs
@@ -0,0 +1,292 @@
+﻿namespace FSharp.Data
+
+module public JsonValidation =
+  open System
+
+  type ValidationResult =
+    | Valid
+    | Invalid of string
+
+  type KeyValidation =
+    | Required of string
+    | Optional of string
+ 
+  type private Description = string
+  type private Predicate = string->bool
+  
+  type StringAttributes =
+    | IsNotEmpty
+    | IsLowerCase
+    | IsUpperCase
+    | IsEmail
+    | IsAlphaNumeric
+    | IsGuid
+    | IsMinimumLength of int
+    | IsMaximumLength of int
+    | MatchesCaseInsensitive of string
+    | MeetsCriteria of Description*Predicate
+
+  type NumberAttributes =
+    | IsPositive
+    | IsNegative
+    | IsNonNegative
+    | IsIntegral
+    | IsGreaterThan of decimal
+    | IsLessThan of decimal
+
+  type ArrayAttributes =
+    | LengthIsAtLeast of int
+    | LengthIsAtMost of int
+    | LengthIsExactly of int
+    | ItemsMatch of JsonSchema
+
+  and JsonSchema =
+    | AnyNumber
+    | NumberThat of NumberAttributes list
+    | AnyString
+    | AnyArray
+    | AnyObject
+    | StringThat of StringAttributes list
+    | ObjectWhere of (KeyValidation * JsonSchema) list
+    | Either of JsonSchema list
+    | ArrayWhose of ArrayAttributes list
+    | Exactly of JsonValue
+    | ExactlyOneOf of JsonValue list
+    | AnythingBut of JsonValue list
+    | Anything
+    | Not of JsonSchema
+    | Delay of (unit -> JsonSchema)
+   
+     
+  let private (|PositiveInteger|NonPositiveInteger|) n =
+    if n > -1
+    then PositiveInteger
+    else NonPositiveInteger
+
+  let nonEmptyString = StringThat [IsNotEmpty]
+
+  let inline (.=) key (schema: JsonSchema) = Required key, schema
+  let inline (.?=) key (schema: JsonSchema) = Optional key, schema
+  let private (!<) (num1: int) (num2: int) = not <| (num1 < num2)
+  let private (!>) (num1: int) (num2: int) = not <| (num1 > num2)
+
+  let inline (-->) choice1 getChoice2 =
+    match choice1 with
+      | Valid -> getChoice2 ()
+      | Invalid error -> Invalid error
+
+  let mapInvalid fn = function
+    | Invalid v -> Invalid <| fn v
+    | Valid -> Valid
+
+  let private matchesRegex regex str =
+    let r = Text.RegularExpressions.Regex(regex)
+    r.IsMatch(str)
+
+  let rec private stringMeetsProperties str props =
+    match str, props with
+      | _, [] -> Valid
+      | "", (IsNotEmpty::_) -> Invalid "Expected string to not be empty"
+      | str, (IsNotEmpty::rest) -> stringMeetsProperties str rest
+      | str, (IsLowerCase::rest) -> isPropertyValid isLowerCase str rest "be completely lowercase"
+      | str, (IsUpperCase::rest) -> isPropertyValid isUpperCase str rest "be completely uppercase"
+      | str, (IsEmail::rest) -> isPropertyValid isEmail str rest "be a valid email address"
+      | str, (IsAlphaNumeric::rest) -> isPropertyValid isAlphaNumeric str rest "be alpha numeric"
+      | str, (IsGuid::rest) -> isPropertyValid isValidGuid str rest "be a valid Guid"
+      | str, (IsMinimumLength len::rest) -> isPropertyValid (isMinimumLength len) str rest (sprintf "be at least %d" len)
+      | str, (IsMaximumLength len::rest) -> isPropertyValid (isMaximumLength len) str rest (sprintf "be at most %d" len)
+      | str, (MatchesCaseInsensitive givenStr::rest) -> isPropertyValid (caseInsensitive givenStr) str rest "be case insensitive" 
+      | str, (MeetsCriteria (description, pred)::rest) -> isPropertyValid pred str rest description
+
+  and private isPropertyValid f str rest description =
+     match f str with 
+      | true -> stringMeetsProperties str rest
+      | false -> Invalid <| sprintf "Expected %s to %s" str description
+
+  and private isLowerCase str = 
+    str.ToLower() = str
+  
+  and private isUpperCase str = 
+    str.ToUpper() = str
+
+  and private caseInsensitive providedStr str =
+    String.Compare(providedStr, str, true) = 0
+
+  and private isEmail =
+    matchesRegex "^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$"
+
+  and private isAlphaNumeric =
+    matchesRegex "^[a-zA-Z0-9]+$"
+
+  and private isValidGuid =
+    Guid.TryParse >> fst
+
+  and private isMinimumLength len str =
+    match len with
+      | PositiveInteger -> (!<) str.Length len
+      | NonPositiveInteger -> false
+
+  and private isMaximumLength len str =
+    match len with
+      | PositiveInteger -> (!>) str.Length len
+      | NonPositiveInteger -> false
+    
+  let rec private numberMeetsProperties num props =
+    match num, props with
+      | _, [] -> Valid
+      | num, IsPositive::rest when num > 0M -> numberMeetsProperties num rest
+      | num, IsNegative::rest when num < 0M -> numberMeetsProperties num rest
+      | num, IsNonNegative::rest when num >= 0M -> numberMeetsProperties num rest
+      | num, IsIntegral::rest when num % 1M = 0M -> numberMeetsProperties num rest
+      | num, IsGreaterThan n::rest when num > n -> numberMeetsProperties num rest
+      | num, IsLessThan n::rest when num < n -> numberMeetsProperties num rest
+      | num, prop::_ -> Invalid <| sprintf "Expected number %f to meet %A property" num prop
+
+  let private isExactlyOneOf literals value =
+    match List.exists (fun expected -> expected = value) literals with
+    | true ->  Valid
+    | false -> Invalid <| sprintf "Expected value to be one of %A but was %A" literals value
+
+  let private isNoneOf literals value =
+    match List.tryFind (fun expected -> expected = value) literals with
+    | None ->  Valid
+    | Some value -> Invalid <| sprintf "Expected value to not be any of %A but was %A" literals value
+
+  let private isExactly expected actual =
+    match expected = actual with
+    | true -> Valid
+    | false -> Invalid <| sprintf "Expected value to be %A but was %A" expected actual
+
+  let rec private gatherRequiredProperties = function
+    | [] -> Map.empty
+    | (Required prop, schema)::rest -> Map.add prop schema <| gatherRequiredProperties rest
+    | _::rest -> gatherRequiredProperties rest
+
+  let rec private gatherOptionalProperties = function
+    | [] -> Map.empty
+    | (Optional prop, schema)::rest -> Map.add prop schema <| gatherOptionalProperties rest
+    | _::rest -> gatherOptionalProperties rest
+
+  let private hasAll props (required: Map<string, 'a>) =
+    let requiredKeys = Map.toList required |> List.map fst
+    let keyExists key = Seq.exists (fun (prop, _) -> prop = key) props
+    match Seq.forall keyExists requiredKeys with
+    | true -> Valid
+    | false -> Invalid <| sprintf "Expected object to have properties %A, but it has %A" (Map.toList required |> Seq.map fst) (props |> Seq.map fst)
+
+  let rec validate (schema : JsonSchema) (json : JsonValue) =
+    try
+      match json, schema with
+        | JsonValue.Float _, AnyNumber
+        | JsonValue.Number _, AnyNumber -> Valid
+        | JsonValue.Number n, NumberThat properties -> numberMeetsProperties n properties 
+        | JsonValue.Float n, NumberThat properties -> numberMeetsProperties (decimal n) properties
+        | invalid, AnyNumber
+        | invalid, NumberThat _ -> Invalid <| sprintf "Expected a number, got %A" invalid
+
+        | JsonValue.String _,  AnyString -> Valid
+        | invalid, AnyString -> Invalid <| sprintf "Expected a string, got %A" invalid
+        | JsonValue.String string, StringThat properties -> stringMeetsProperties string properties
+        | invalid, StringThat _ -> Invalid <| sprintf "Expected a string, got %A" invalid
+
+        | value, ExactlyOneOf literals -> isExactlyOneOf literals value
+        | value, AnythingBut literals -> isNoneOf literals value
+        | value, Exactly literal -> isExactly literal value
+
+        | JsonValue.Record properties, ObjectWhere propSchema -> propertiesMeetSchema properties propSchema
+        | invalid, ObjectWhere _ -> Invalid <| sprintf "Expected an object, got %A" invalid
+        | JsonValue.Record _, AnyObject -> Valid
+        | invalid, AnyObject -> Invalid <| sprintf "Expected an object, got %A" invalid
+
+        | JsonValue.Array items, ArrayWhose properties -> arrayMeetsProperties items properties
+        | invalid, ArrayWhose _ -> Invalid <| sprintf "Expected an array, got %A" invalid
+        | JsonValue.Array _, AnyArray -> Valid
+        | invalid, AnyArray -> Invalid <| sprintf "Expected an array, got %A" invalid
+
+        | value, Either schemas -> validateEither value schemas
+
+        | value, Not schema -> negateValidation schema value
+
+        | _, Anything -> Valid
+
+        | value, Delay getSchema -> validate (getSchema ()) value
+    with
+    | ex -> Invalid <| ex.Message
+
+  and private negateValidation schema value =
+    match validate schema value with
+      | Valid -> Invalid <| sprintf "Expected %A not to match schema %A" value schema
+      | Invalid _ -> Valid
+
+  and private requiredValuesMeetSchemas (requiredLookup : Map<string, JsonSchema>) props  =
+    let formatErrorUnder = sprintf ".%s %s"
+    let validateEach result (key, value) =
+      result --> fun () -> mapInvalid (formatErrorUnder key) <| validate requiredLookup.[key] value
+
+    props
+      |> Array.filter (requiredLookup.ContainsKey << fst)
+      |> Array.fold validateEach Valid
+
+  and private optionalValuesMeetSchemas (optionalLookup : Map<string, JsonSchema>) props =
+    let formatErrorUnder = sprintf ".%s %s"
+    let validateEach result (key, value) =
+      result --> fun () -> mapInvalid (formatErrorUnder key) <| validate optionalLookup.[key] value
+
+    props
+      |> Array.filter (optionalLookup.ContainsKey << fst)
+      |> Array.fold validateEach Valid
+
+  and private propertiesMeetSchema properties propSchema =
+    let requiredLookup = gatherRequiredProperties propSchema
+    let optionalLookup = gatherOptionalProperties propSchema
+
+    hasAll properties requiredLookup
+      --> fun () -> requiredValuesMeetSchemas requiredLookup properties
+      --> fun () -> optionalValuesMeetSchemas optionalLookup properties
+
+  and private arrayMeetsProperties items props =
+    match props with
+      | [] -> Valid
+      | LengthIsAtLeast n::rest ->
+        if items.Length >= n then arrayMeetsProperties items rest
+        else Invalid <| sprintf "Expected array to have at least %d item(s), but it had %d" n items.Length
+
+      | LengthIsAtMost n::rest ->
+        if items.Length <= n then arrayMeetsProperties items rest
+        else Invalid <| sprintf "Expected array to have at most %d item(s), but it had %d" n items.Length
+
+      | LengthIsExactly n::rest ->
+        if items.Length = n then arrayMeetsProperties items rest
+        else Invalid <| sprintf "Expected array to have exactly %d item(s), but it had %d" n items.Length
+
+      | ItemsMatch schema::rest ->
+        match validateAll 0 (Array.toList items) schema with
+          | Valid -> arrayMeetsProperties items rest
+          | Invalid error -> Invalid error
+
+  and private validateAll i items schema =
+    let formatErrorUnder = sprintf "[%d] %s"
+
+    match items with
+      | [] -> Valid
+      | item::rest ->
+        match validate schema item with
+          | Valid -> validateAll (i + 1) rest schema
+          | Invalid error -> mapInvalid (formatErrorUnder i) <| Invalid error
+
+  and private validateEither value schemas  =
+    let rec go errorAcc = function
+      | [] -> errorAcc
+      | schema::rest ->
+        match validate schema value with
+          | Valid -> []
+          | Invalid error -> go (error::errorAcc) rest
+
+    let errors = go [] schemas
+
+    match errors with
+      | [] -> Valid
+      | invalids ->
+        invalids
+          |> String.concat " (or) "
+          |> Invalid
